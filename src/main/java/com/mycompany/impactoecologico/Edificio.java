93473af6dbafbd54bdb35c0574494554eeafbd3c
JORGE <jlopez5299@utm.edu.ec>
2023-11-26 08:21:07 -0500

    Clase Edificio

---
+++ src/main/java/com/mycompany/impactoecologico/Edificio.java
@@ -0,0 +1,26 @@
+public class Edificio implements ImpactoEcologico {
+    private double emisionesConstruccion;
+    private String tipoEnergia;
+    private double eficienciaEnergia;
+    private double mantenimientoAnual;
+    private int vidaUtil;
+    private double impactoDemolicion;
+
+    public Edificio(double emisionesConstruccion, String tipoEnergia, 
+    double eficienciaEnergia, double mantenimientoAnual, int vidaUtil, 
+    double impactoDemolicion) {
+        this.emisionesConstruccion = emisionesConstruccion;
+        this.tipoEnergia = tipoEnergia;
+        this.eficienciaEnergia = eficienciaEnergia;
+        this.mantenimientoAnual = mantenimientoAnual;
+        this.vidaUtil = vidaUtil;
+        this.impactoDemolicion = impactoDemolicion;
+    }
+
+    @Override
+    public double calcularImpactoEcologico() {
+        // Esbozo de lógica de cálculo
+        return emisionesConstruccion + (eficienciaEnergia * mantenimientoAnual) + (impactoDemolicion / vidaUtil);
+    }
+
+}
